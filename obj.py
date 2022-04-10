d5526d125cc9add65bb4cb9908aec2874d5deee3
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-04-10 15:21:08 -0300

    Update obj.py

--- obj.py
+++ obj.py
@@ -5,19 +5,13 @@ class Obj:
 
     def __init__(self, image, x, y):
 
-        self.group = pygame.sprite.Group()  
-                                            
-                                            
-        self.sprite = pygame.sprite.Sprite(self.group)  
-
-                                                       
+        self.group = pygame.sprite.Group()                                                                                     
+        self.sprite = pygame.sprite.Sprite(self.group)                                                       
         self.sprite.image = pygame.image.load(image)    
         self.sprite.rect = self.sprite.image.get_rect()
         self.sprite.rect[0] = x
         self.sprite.rect[1] = y
-
         self.frame = 1                                  
-
         self.tick = 0
 
     def draw(self, window):
@@ -27,34 +21,25 @@ class Obj:
         self.tick += 1
         if self.tick == tick:
             self.tick = 0
-            self.frame += 1
-                                                        
-        
+            self.frame += 1                                                     
         if self.frame == frames: #4:
             self.frame = 1
                                                         
         self.sprite.image = pygame.image.load("assets/"+ image + str(self.frame) +".png")
 
-                                
-        
-'''
 class Bee(Obj):
 
     def __init__(self, image, x, y):
         super().__init__(image, x, y)
                                                                     
-
         pygame.mixer.init()                                         
         self.sound_pts = pygame.mixer.Sound("assets/sounds/score.ogg")
-        self.sound_block = pygame.mixer.Sound("assets/sounds/bateu.ogg")
-                                                                    
-        
+        self.sound_block = pygame.mixer.Sound("assets/sounds/bateu.ogg")                                                                        
         self.life = 3                                               
         self.pts = 0                                                
         
     def move_bee(self, event):
-        if event.type == pygame.MOUSEMOTION:                        
-            
+        if event.type == pygame.MOUSEMOTION:                         
             self.sprite.rect[0] = pygame.mouse.get_pos()[0] - 35    
             self.sprite.rect[1] = pygame.mouse.get_pos()[1] - 30    
 
@@ -62,21 +47,15 @@ class Bee(Obj):
         name = name
         colision = pygame.sprite.spritecollide(self.sprite, group, True)
                                                 
-
-        if name == "Flower" and colision:
-            
+        if name == "Flower" and colision:            
             self.pts += 1                       
             self.sound_pts.play()               
             print(self.pts)
-        elif name == "Spider" and colision:
-            
+        elif name == "Spider" and colision:         
             self.life -= 1                      
             self.sound_block.play()             
             print(self.life)
-                                                
-
-                                                
-
+                                                                                              
 class Text:
 
     def __init__(self, size, text):
@@ -89,7 +68,4 @@ class Text:
         window.blit(self.render, (x, y))
 
     def update_text(self, update):
-        self.render = self.font.render(update, False, (255, 255, 255))
-'''
-       
-        
+        self.render = self.font.render(update, False, (255, 255, 255))       
