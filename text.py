3f37aeef760140ccbf311b4ef3d422330697d61a
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-04-10 14:06:11 -0300

    Update text.py

--- text.py
+++ text.py
@@ -4,12 +4,8 @@ import config as cf
 class Text:
 
     def __init__(self, size, text):
-
         self.font = pygame.font.SysFont("Arial bold", size)
-        self.render = self.font.render(text, False, cf.COLOR)
-                                                #a variavel self.render é usada
-                                                #para colocar o texto que vai
-                                                #para a tela.        
+        self.render = self.font.render(text, False, cf.COLOR)       
 
     def draw(self, window, x, y):
         window.blit(self.render, (x, y))
