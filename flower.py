7da08107a1e788a71736351a776470fb8f1d3c65
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-04-11 01:25:13 -0300

    Update flower.py

--- flower.py
+++ flower.py
@@ -11,6 +11,6 @@ class Flower(NPMobileObj):
         self.sprite.rect[1] += cf.TAX_UPDATE_Y_FLOWER
         if self.sprite.rect[1] > cf.LIM_Y_FLOWER:
             self.sprite.kill ()
-            self.__init__ ("assets/florwer1.png", random.randrange (0, cf.SIZE_WINDOW_X - 40),
+            self.__init__ ("assets/flower1.png", random.randrange (0, cf.SIZE_WINDOW_X - 40),
                            cf.INITIAL_POSITION_FLOWER_Y)
 
