3b8eba767c675e0d419c5d4bcd6eff2f54550902
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2022-04-11 02:01:33 -0300

    Update np_mobile_obj.py

--- np_mobile_obj.py
+++ np_mobile_obj.py
@@ -1,16 +1,13 @@
 from obj import Obj
 
 
-
-
+#For Moving Objects
 class NPMobileObj(Obj):
     def __init__(self, image, x, y):
         super().__init__(image, x, y)
 
     def movement(self, speed_var):
         self.sprite.rect[1] += speed_var
-
-
 
     def reposition(self, initial_position_y, final_position_y):
         if self.sprite.rect[1] >= initial_position_y:
